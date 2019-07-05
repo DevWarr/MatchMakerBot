d8d20985256ac56cf14729a46ea0d862e44dc128
Devin Warrick <dev.warr@gmail.com>
2019-07-05 18:17:08 -0500

    Bot deletes all messages, then creates a new one with reaction emojis.

--- commands/ready.js
+++ commands/ready.js
@@ -1,18 +1,31 @@
 exports.run = client => {
     console.log("Fetching messages...")
-    const botTestTwo = client.channels.find(
+    const matchmaking = client.channels.find(
         channel => channel.id === "596803653055021087"
     )
+    matchmaking
+        .fetchMessages()
+        .then(messages => {
+            messages.forEach(message => message.delete())
+        })
+        .then(() => {
+            const ping = "🔔"
+            const noPing = "🔕"
+            console.log(ping, noPing)
+            matchmaking
+                .send(
+                    `Click on an emoji to determine your status!\n\n${ping} to receive pings\n${noPing} to receive _no_ pings\nTest it out!`
+                )
+                .then(msg => {
+                    msg.react(ping)
+                    msg.react(noPing)
+                })
+        })
+        .catch(console.error)
+
     const freeBotTesting = client.channels.find(
         channel => channel.id === "596809535419711502"
     )
-    botTestTwo.fetchMessages().then(messages => {
-        messages.forEach(message => {
-            if (!message.author.bot) {
-                message.delete()
-            }
-        })
-    })
     freeBotTesting.fetchMessages().then(messages => {
         messages.forEach(message => {
             if (!message.author.bot) {
@@ -20,6 +33,7 @@ exports.run = client => {
             }
         })
     })
+
     console.log("Messages fetched!")
     console.log("I am ready!")
 }
