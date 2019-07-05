c4db0ae9879c1c6fd53379874d9b1f7f9eaae0b8
Devin Warrick <dev.warr@gmail.com>
2019-07-05 17:35:37 -0500

    On load, fetches messages for specified channels and deletes messages not created by a bot.

--- commands/ready.js
+++ commands/ready.js
@@ -1,3 +1,25 @@
 exports.run = client => {
+    console.log("Fetching messages...")
+    const botTestTwo = client.channels.find(
+        channel => channel.id === "596803653055021087"
+    )
+    const freeBotTesting = client.channels.find(
+        channel => channel.id === "596809535419711502"
+    )
+    botTestTwo.fetchMessages().then(messages => {
+        messages.forEach(message => {
+            if (!message.author.bot) {
+                message.delete()
+            }
+        })
+    })
+    freeBotTesting.fetchMessages().then(messages => {
+        messages.forEach(message => {
+            if (!message.author.bot) {
+                message.delete()
+            }
+        })
+    })
+    console.log("Messages fetched!")
     console.log("I am ready!")
 }
