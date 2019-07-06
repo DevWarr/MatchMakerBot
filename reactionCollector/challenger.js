e16ba9dbc40ebc1849a2f08115b44f9579e4941c
Devin Warrick <dev.warr@gmail.com>
2019-07-06 02:35:07 -0500

    Reactions work properly down to channel creation.

--- reactionCollector/challenger.js
+++ reactionCollector/challenger.js
@@ -42,15 +42,83 @@ exports.run = (client, botId, reactions, user1, user2, reaction, user) => {
     // Final error check
     if (reaction.name !== ok) reaction.remove(user.id).catch(console.error)
 
+    // Alright, step one is to remove the extra messages, and set both users' roles
     msgRemover(msg.channel, user1, user2)
     roleReset(member1, Object.keys(variables.roles))
     roleReset(member2, Object.keys(variables.roles))
     member1.addRole(role).catch(console.error)
     member2.addRole(role).catch(console.error)
-    msg.guild.createChannel("the-matchmaker-group", {type: "category"})
+
+    // Then we need to create our new channel:
+
+    // Get our users' full object so we have their username
+    user1 = msg.guild.members.find(member => member.user.id === user1).user
+    user2 = msg.guild.members.find(member => member.user.id === user2).user
+
+    // To create a channel name, we shall do this!
+    const channelName = `${user1.username}-vs-${user2.username}`
+        .replace(/[^a-z0-9-_]/gi, "")
+        .toLowerCase()
+
+    // Our main category channel
+    const channelCategory = msg.guild.channels.get(
+        variables.channels.matchesGroup
+    )
+    channelCategory
+        .edit({
+            permissionOverwrites: [
+                {
+                    id: msg.guild.id,
+                    deny: ["VIEW_CHANNEL"]
+                },
+                { id: user1, allow: ["VIEW_CHANNEL"] },
+                { id: user2, allow: ["VIEW_CHANNEL"] }
+            ]
+        })
+        .catch(console.error)
+    console.log(channelCategory.id)
+
+    // The text channel
+    const textChannel = msg.guild
+        .createChannel(`text- ${channelName}`, {
+            type: "text",
+            permissionOverwrites: [
+                {
+                    id: msg.guild.id,
+                    deny: ["VIEW_CHANNEL"]
+                },
+                { id: user1, allow: ["VIEW_CHANNEL"] },
+                { id: user2, allow: ["VIEW_CHANNEL"] }
+            ]
+        })
+        .then(
+            channel => channel.setParent(channelCategory.id).catch(console.log) // appended to the category
+        )
+        .catch(console.log)
+
+    // The voice channel
+    const voiceChannel = msg.guild
+        .createChannel(`voice-${channelName}`, {
+            type: "voice",
+            permissionOverwrites: [
+                {
+                    id: msg.guild.id,
+                    deny: ["VIEW_CHANNEL"]
+                },
+                { id: user1, allow: ["VIEW_CHANNEL"] },
+                { id: user2, allow: ["VIEW_CHANNEL"] }
+            ]
+        })
+        .then(
+            channel => channel.setParent(channelCategory.id).catch(console.log) // appended to the category
+        )
+        .catch(console.log)
+
     msg.channel
         .send(
-            `<@${user1}> vs <@${user2}> now in progress. Please proceed to DMs if you wish to communicate.\nThe match has begun!\n\n(Thank you for using <@${
+            `<@${user1.id}> vs <@${
+                user2.id
+            }> now in progress at #${channelName}. Please proceed there to communicate.\nThe match has begun!\n\n(Thank you for using <@${
                 variables.users.bot
             }>!)`
         )
