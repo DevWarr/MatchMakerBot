3a07788da4884d5d633e7449862700c7acf5e063
Devin Warrick <dev.warr@gmail.com>
2019-07-06 19:25:18 -0500

    msgRemover.

---
+++ commands/msgRemover.js
@@ -0,0 +1,8 @@
+// Removes Matchmaking messages.
+const variables = require("../utils/variables.js")
+
+exports.run = (channel, user1, user2 = null) => {
+    channel.messages.forEach(message => {
+        if (!message.mentions.users.get(variables.users.bot) && (message.mentions.users.get(user1) || message.mentions.users.get(user2))) message.delete().catch(console.log)
+    })
+}
